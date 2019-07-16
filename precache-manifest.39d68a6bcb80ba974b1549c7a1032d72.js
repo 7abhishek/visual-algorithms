01193ecda98073dea6c1108b411f6fb801ef0009
Abhishek <7abhishek.25@gmail.com>
2019-07-16 21:37:36 +0530

    Updates

---
+++ precache-manifest.39d68a6bcb80ba974b1549c7a1032d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b34386138fbaffc428d883fa399dc1a",
+    "url": "/visual-algorithms/index.html"
+  },
+  {
+    "revision": "20b202bc3e3e5d70909f",
+    "url": "/visual-algorithms/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0eb5d88925466b480a95",
+    "url": "/visual-algorithms/static/js/2.8c528cd2.chunk.js"
+  },
+  {
+    "revision": "20b202bc3e3e5d70909f",
+    "url": "/visual-algorithms/static/js/main.5e2d676c.chunk.js"
+  },
+  {
+    "revision": "8b2e72c03b34d39bf131",
+    "url": "/visual-algorithms/static/js/runtime~main.0b739596.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/visual-algorithms/static/media/logo.5d5d9eef.svg"
+  }
+]);
